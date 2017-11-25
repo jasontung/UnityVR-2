2b74ca9465e47a726b0b67f9798a365c69b5b115
4419-TEACHER\user <jasontung2214@gmail.com>
2017-11-25 09:35:56 +0800

    1. 完成基本射線效果

--- Assets/Scripts/ShootingGunController.cs
+++ Assets/Scripts/ShootingGunController.cs
@@ -1,9 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using VRStandardAssets.Utils;
 using UnityEngine.VR;
+using VRStandardAssets.Utils;
+
 public class ShootingGunController : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public VRInput vrInput;
+    public ParticleSystem flareParticle;
+    public LineRenderer gunFlare;
+    public Transform gunEnd;
+    public float defaultLineLength = 70f;
+    public float gunFlareVisibleSeconds = 0.07f;
+    private void OnEnable()
+    {
+        vrInput.OnDown += HandleDown;
+    }
 
+    private void OnDisable()
+    {
+        vrInput.OnDown -= HandleDown;
+    }
+
+    private void HandleDown()
+    {
+        StartCoroutine(Fire());
+    }
+
+    private IEnumerator Fire()
+    {
+        audioSource.Play();
+        float lineLength = defaultLineLength;
+        //TODO 判斷有無射到東西
+        flareParticle.Play();
+        gunFlare.enabled = true;
+        yield return StartCoroutine(MoveLineRenderer(lineLength));
+        gunFlare.enabled = false;
+    }
+
+    private IEnumerator MoveLineRenderer(float lineLength)
+    {
+        float timer = 0f;
+        while(timer < gunFlareVisibleSeconds)
+        {
+            gunFlare.SetPosition(0, gunEnd.position);
+            gunFlare.SetPosition(1, gunEnd.position + gunEnd.forward * lineLength);
+            yield return null;
+            timer += Time.deltaTime;
+        }
+    }
 }
